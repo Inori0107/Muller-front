0fe73a8ec8b8dfa79c83010cabd7e309c6c7d6e0
InoriLin0107 <InoriLin0107@gmail.com>
2024-10-09 01:16:03 +0800

    ping

--- src/composables/axios.js
+++ src/composables/axios.js
@@ -22,33 +22,40 @@ apiAuth.interceptors.response.use(
     return res;
   },
   async (error) => {
-    // 如果失敗有回應 (網路斷線也算是失敗，但網路斷線不會有回應)
     if (error.response) {
-      // 如果得到登入過期的回應訊息，且不是舊換新
       if (
         error.response.data.message === "登入過期" &&
         error.config.url !== "/user/extend"
       ) {
         const user = useUserStore();
         try {
-          // 傳送舊換新的請求
           const { data } = await apiAuth.patch("/user/extend");
-          // 舊換新成功 更新 store 的 token
           user.token = data.result;
-          // 修改原本發生錯誤的請求設定
           error.config.headers.Authorization = "Bearer " + user.token;
-          // 重新傳送一次原本的請求
           return axios(error.config);
         } catch (_) {
-          // 舊換新錯誤 登出
           user.logout();
         }
       }
     }
-    // 回傳原本請求的錯誤到呼叫 apiAuth 的地方
     return Promise.reject(error);
   }
 );
+
+// 新增 pingServer 函式以防止後端進入休眠狀態
+function pingServer() {
+  api
+    .get("/ping")
+    .then(() => {
+      console.log("Ping success");
+    })
+    .catch((error) => {
+      console.error("Ping failed:", error);
+    });
+}
+
+// 每 5 分鐘 (300000 毫秒) ping 一次伺服器
+setInterval(pingServer, 300000);
 
 export const useApi = () => {
   return { api, apiAuth };
